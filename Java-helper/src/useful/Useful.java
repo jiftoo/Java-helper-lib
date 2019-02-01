9494d7f1067a3a81a14e78b435d7985b4243a8e9
0x666c <0x666c@mail.ru>
2019-02-02 01:06:21 +0300

    Improved documentation and corrected some typos
    ^

--- Java-helper/src/useful/Useful.java
+++ Java-helper/src/useful/Useful.java
@@ -37,7 +37,7 @@ public class Useful {
 	 * @param place	Specify the place in the sequence (e.g. if place equals 10 the method will return 55)
 	 * @return      The fibonacci number in the specified place
 	 */
-	public static long find (long place) {
+	public static long findFibonacciAt (long place) {
 
 		long value = place;
 
@@ -78,31 +78,54 @@ public class Useful {
 	}
 	/**
 	 * Centers your Frame on screen
+	 * Does not use frame.setLocationRelativeTo(null)
 	 * @param frame The name of your <i>Frame</i> variable
 	 */		
-	public static void center(Frame frame) {
+	public static void placeAtCenter(Frame frame) {
 		Dimension screenSize = (Toolkit.getDefaultToolkit().getScreenSize());
 		int Dx = (int) screenSize.getWidth();
 		int Dy = (int) screenSize.getHeight();
 		frame.setLocation(Dx/2 - frame.getSize().width/2, Dy/2 - frame.getSize().height/2-40);
 
 	}
-	public static void center(Frame frame, int width, int height) {
+	/**
+	 * Centers your Frame on screen
+	 * Does the same thing as frame.setLocationRelativeTo(null)
+	 * @param frame The name of your <i>Frame</i> variable
+	 */
+	public static void center(Frame frame) {
+		frame.setLocationRelativeTo(null);
+	}
+	/**
+	 * Centers your Frame on screen and resizes if (idk why i added this)
+	 * Does not use frame.setLocationRelativeTo(null)
+	 * @param frame The name of your <i>Frame</i> variable
+	 * @param width Desired width
+	 * @param height Desired height
+	 */
+	public static void centerAndResize(Frame frame, int width, int height) {
 		Dimension screenSize = (Toolkit.getDefaultToolkit().getScreenSize());
 		int Dx = (int) screenSize.getWidth();
 		int Dy = (int) screenSize.getHeight();
 		frame.setBounds(new Rectangle(Dx/2 - frame.getSize().width/2, Dy/2 - frame.getSize().height/2-40, width, height));
 
 	}
+	/**
+	 * Returns your ip adress
+	 * 
+	 * @param site Optional. A site that will ONLY contain your ip or any other string.
+	 * if set to 'null', the function reaches "https://api.ipify.org/" to get the ip.
+	 */
 	public static String getIp(String site) throws Exception {
 		String tmp = null;
 		try {
+			if(site = null) throw new Exception();
+			
 			Scanner sc = new Scanner(new URL(site).openStream(),"UTF-8");
 			tmp=sc.next();
 			sc.close();
 			return tmp;
-		}catch (Exception e1) {
-			System.out.println("An error occured! Retrying with preset site...");
+		} catch (Exception e1) {
 			try {
 				Scanner sc1 = new Scanner(new URL("https://api.ipify.org/").openStream(),"UTF-8");
 				tmp=sc1.next();
@@ -134,31 +157,53 @@ public class Useful {
 		});
 	}
 	/**
-	 * 
-	 * @param label
-	 * @param text
+	 * Sets text color of JLabel to red and
+	 * als sets text to provided string
+	 * @param label Labet to use
+	 * @param text Desired text
 	 */
 	public static void errorLabel(JLabel label, String text) {
 		label.setForeground(Color.RED);
 		label.setText(text);
 	}
+	/**
+	 * Sets text color of JLabel to green and
+	 * als sets text to provided string
+	 * @param label Labet to use
+	 * @param text Desired text
+	 */
 	public void successLabel(JLabel label, String text) {
 		label.setForeground(Color.GREEN);
 		label.setText(text);
 	}
 	
-	public static void easySleep(long milis) {
+	/**
+	 * Probably the most useful function
+	 * 
+	 * Does the same as Thread.sleep(int millis), but keeps your code cleaner
+	 * as you don't need to catch an InterruptedException.
+	 * @param millis Amount of milliseconds to sleep
+	 */
+	public static void easySleep(long millis) {
 		try {
-			Thread.sleep(milis);
+			Thread.sleep(millis);
 		} catch (InterruptedException e) {
-			System.err.println("The thread was interrupted!\nIf it was dow on purpose, please use regular sleep method");
+			System.err.println("The thread was interrupted!\nIf it was done on purpose, please use regular sleep method");
 		}
 	}
-	public static void easySleep(long milis, int nanos) {
+	/**
+	 * Probably the most useful function
+	 * 
+	 * Does the same as Thread.sleep(long millis, int nanos), but keeps your code cleaner
+	 * as you don't need to catch an InterruptedException.
+	 * @param millis Amount of milliseconds to sleep
+	 * @param nanos Amount of nanoseconds to sleep
+	 */
+	public static void easySleep(long millis, int nanos) {
 		try {
-			Thread.sleep(milis, nanos);
+			Thread.sleep(millis, nanos);
 		} catch (InterruptedException e) {
-			System.err.println("The thread was interrupted!\nIf it was dow on purpose, please use regular sleep method");
+			System.err.println("The thread was interrupted!\nIf it was done on purpose, please use regular sleep method");
 		}
 	}
 }
